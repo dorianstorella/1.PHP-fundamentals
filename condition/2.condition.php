c9d1fd3223170d7cab588abe15e759ea9d467849
Dorian Storella <dorianstorella@gmail.com>
2020-11-12 10:36:16 +0100

    ajout heure

--- condition/2.condition.php
+++ condition/2.condition.php
@@ -9,8 +9,11 @@
     <h1>2.Display a different greeting message depending on the time of the day.</h1>
 
     <?php
-    $heure=4.59; 
-    if($heure <= 9 AND $heure>= 5) {
+    $heure=date('h:i:s');       // $date = date('d-m-y h:i:s');  <== gauche date    droite heure
+                                    //c est la function date()              src = https://www.tutorialrepublic.com/faq/how-to-get-the-current-date-and-time-in-php.php#:~:text=Answer%3A%20Use%20the%20PHP%20date%20%28%29%20Function%20You,following%20example%20to%20see%20how%20it%20basically%20works%3A
+    echo $heure ;                 //pour appeler une function
+
+    if($heure <= 9 AND $heure>= 5) {        
     echo 'good morning!';
 }
     if($heure > 9 AND $heure <= 12)
