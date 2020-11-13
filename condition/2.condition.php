cd53699936c31345b9fe1eeae2ce73ec161f6a65
Dorian Storella <dorianstorella@gmail.com>
2020-11-13 15:05:37 +0100

    rectification heure

--- condition/2.condition.php
+++ condition/2.condition.php
@@ -8,28 +8,30 @@
 <body>
     <h1>2.Display a different greeting message depending on the time of the day.</h1>
 
+    
     <?php
-    $heure=date('h:i:s');       // $date = date('d-m-y h:i:s');  <== gauche date    droite heure
+    date_default_timezone_set('europe/paris');
+    $heure=date('H:i');       // $date = date('d-m-y h:i:s');  <== gauche date    droite heure
                                     //c est la function date()              src = https://www.tutorialrepublic.com/faq/how-to-get-the-current-date-and-time-in-php.php#:~:text=Answer%3A%20Use%20the%20PHP%20date%20%28%29%20Function%20You,following%20example%20to%20see%20how%20it%20basically%20works%3A
     echo $heure ;                 //pour appeler une function
 
-    if($heure <= 9 AND $heure>= 5) {        
+    if($heure <= 9 AND $heure>= 5){         
     echo 'good morning!';
 }
-    if($heure > 9 AND $heure <= 12)
+    elseif($heure > 9 AND $heure <= 12){
     echo 'good day ';
-
-    if ($heure > 12 AND $heure <= 16)
+}
+    elseif ($heure > 12 AND $heure <= 16){
     echo 'good afternoon';
-
-    if ($heure > 16 AND $heure >= 21)
+}
+    elseif ($heure > 16 AND $heure >= 21){
     echo 'good evening';
-
-    if ($heure > 21 )
+}
+    elseif ($heure > 21 ){
     echo 'good night';
-
-    if ($heure <= 4.59)
+}
+    elseif ($heure <= 4.59){
     echo 'good night';
-    ?>    
+}    ?>    
 </body>
 </html>
