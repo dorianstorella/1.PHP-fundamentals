574dece85894827d3648576bc1b5b292bd32b5f4
Dorian Storella <dorianstorella@gmail.com>
2020-11-13 09:48:23 +0100

    ajout de l ex 1 et ajout ex 7 non fini

---
+++ condition/7.condition.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Achieve the same, without the ELSE.</h1>
+    
+    <form method="get" action="">
+    <label for="age">enter your age please</label>
+    <input type="text" name="age">
+    <br>
+    <label for="genre">what's you gender?</label>
+    
+    <label for="male">male</label>
+    <input type="radio" name="genre" value="male">
+    
+    <label for="women">women</label>
+    <input type="radio" name="genre"  value="women">
+    <br>
+    <label for="name">enter you name</label>
+    <input type="text" name="firstname">
+
+    <input type="submit" name="submit" value="click">
+    </form>
+    
+
+    <?php
+    if (isset($_GET['age'])){
+    $age=$_GET['age'];
+    } else { 
+        $age='';
+    }
+   //echo $age
+
+    if (isset($_GET['genre'])){
+       $genre=$_GET['genre']; 
+    } else {
+        $genre='';
+
+    }
+    if (isset($_GET['firstname'])){
+       $name=$_GET['firstname'];
+    } else {
+       $name='';
+    }
+  
+
+    if ( $age >= 21 AND $age <=40 AND $genre === 'women' ){
+    echo 'welcome to the team' .' '. $name;
+    }
+
+    if ( $age >=21 AND $age <=40 AND $genre ==='male'  ){
+    echo "sorry you don't have criteria" .' '. $name;
+    }
+    
+    if ( $age < 21 AND $age <=40 AND $genre ==='women'  ){
+        echo "sorry you don't have criteria" .' '. $name;
+    }
+
+    if ( $age >= 21 AND $age >40 AND $genre ==='women'  ){
+            echo "sorry you don't have criteria" .' '. $name;
+    }
+
+
+   ?>
+</body>
+</html>
