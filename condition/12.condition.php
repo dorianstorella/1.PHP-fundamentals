1e0df67ed405b35919143e0bb8d6b066716026e5
Dorian Storella <dorianstorella@gmail.com>
2020-11-16 16:37:51 +0100

    ajout ex12 non fini

---
+++ condition/12.condition.php
@@ -0,0 +1,43 @@
+In a new file unicorn.php, create an html form asking "Are you a human, a cat or a unicorn ?".
+ When submitted, the screen displays an animated Gif showing either a human, a cat or a unicorn, 
+ as per the user input (you can find gifs here).
+ Use GET or POST as method, whichever you like. Use a ternary operation to evaluate the condition.
+
+ <h1>12. ternary operators</h1>
+
+ <form action="" method="get">
+ <label for="question">Are you a human, a cat or a unicorn ? </label>
+
+ <label for="human">human</label>
+ <input type="radio" name="question" value="human">
+
+ <label for="cat">cat</label>
+ <input type="radio" name="question" value="cat">
+
+ <label for="unicorn">unicorn</label>
+ <input type="radio" name="question" value="unicorn">
+
+ <input type="submit" value="click">
+ </form>
+ 
+
+ <?php
+ if (isset($_GET['question'])){
+     $resp=$_GET['question'];
+     //echo $resp;
+     if ($resp == 'cat'){
+      echo '<iframe src="https://giphy.com/embed/BzyTuYCmvSORqs1ABM" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/hallmarkecards-cute-hallmark-shoebox-BzyTuYCmvSORqs1ABM">via GIPHY</a></p>';
+     }
+     
+ }
+//$file =fopen("https://media.giphy.com/media/BzyTuYCmvSORqs1ABM/giphy.gif" , "x" );
+//imagecreatefromgif (string $file) : resource
+ 
+
+
+//echo '<iframe src="https://giphy.com/embed/BzyTuYCmvSORqs1ABM" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/hallmarkecards-cute-hallmark-shoebox-BzyTuYCmvSORqs1ABM">via GIPHY</a></p>';
+//echo '<iframe src="https://giphy.com/embed/26vUw5sYGcqmMDoBy" width="480" height="346" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/icebreakers-unicorn-laser-ice-breakers-26vUw5sYGcqmMDoBy">via GIPHY</a></p>';
+//echo '<iframe src="https://giphy.com/embed/MSgwmvbfUFZji35z7p" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/hello-hi-women-MSgwmvbfUFZji35z7p">via GIPHY</a></p>';              
+ 
+ ?>
+ 
