316d3e9fa82782d91a2beed4bbfd48ef1036ca3f
Dorian Storella <dorianstorella@gmail.com>
2020-11-17 12:07:43 +0100

    ajout ex1 array

---
+++ array/1.array.php
@@ -0,0 +1,14 @@
+<h1>exercice 1 </h1>
+
+<?php
+
+$famille = array('maman' , 'papa' , 'soeur' );
+ print_r($famille);
+
+$recipe = array('sucre' , 'eau' , 'lait'); 
+print_r($recipe);
+
+$favFilm = array('die hard' , 'tarzan' , 'la planete au tresor');
+print_r($favFilm[2]);
+
+?>
