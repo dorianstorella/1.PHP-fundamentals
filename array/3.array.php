5e1bdac6b1cdb4760e060fba37b7897bcd4892c4
Dorian Storella <dorianstorella@gmail.com>
2020-11-17 14:58:59 +0100

    ajout ex 3 array

---
+++ array/3.array.php
@@ -0,0 +1,36 @@
+
+<h1>ex 3 array</h1>
+
+<?php
+$me  =array(
+    'prenom' => 'dorian' ,
+    'nom' => 'storella',
+    'saison_preferee' => 'été', //season //summer
+    'aime_foot' => 'oui',
+    //'hobbies' => array('walk' , 'code' , 'game' )
+
+);
+
+$me ['hobbies']=array(
+    '5' => 'walk' , 
+    '3' => 'code' , 
+    '1' => 'game' ,
+);
+    echo '<pre>';
+    print_r($me);
+    echo '</pre>';
+
+$mother = array(
+    'firstname'=>'Isabelle',
+    'name'=>'storella',
+    'favourite_season'=> 'spring', // <= printemps && autonne => autumn
+    'hobbies' => array( 
+        '1 hobbies'=>'manucure',
+        '2 hobbies'=>'restaurant' , 
+        '3 hobbies'=>'game' 
+        )
+);
+    echo '<pre>';
+    print_r($mother);
+    echo '</pre>';
+?>
